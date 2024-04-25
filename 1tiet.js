188ab5323ab8eb62fbb208328b2840b7252b6a94
thebao13 <bao.pt.64cntt@ntu.edu.vn>
2024-04-25 20:28:35 +0700

    tinh BMI

--- 1tiet.js
+++ 1tiet.js
@@ -10,5 +10,18 @@ function ketqua(){
         alert("Vui lòng nhập chiều cao");
         return false;
     } 
-   
+    if (isNaN(KG) || isNaN(CM)) {
+        if (isNaN(KG)) {
+            alert("Vui lòng nhập cân nặng là số.");
+        }
+        if (isNaN(CM)) {
+            alert("Vui lòng nhập chiều cao là số.");
+        }
+        return false;
+    }
+    // tính chỉ số BMI
+    var bmi = KG / Math.pow(CM / 100, 2);
+    BMI.value = bmi.toFixed(2);
+
+    return true;
 }
